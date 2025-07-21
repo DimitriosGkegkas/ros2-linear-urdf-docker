fbdec8d43b07a8fc413270042515a63a99059c8f
Dimitris <dimitrisgegas01@gmail.com>
2025-07-21 12:15:11 +0900

    Created Client that reads params A, b from yaml config file

--- src/linear_algebra_service/src/client.cpp
+++ src/linear_algebra_service/src/client.cpp
@@ -1,10 +1,70 @@
-#include <cstdio>
+#include <chrono>
+#include <functional>
+#include <string>
+#include <vector>
 
-int main(int argc, char ** argv)
+#include <rclcpp/rclcpp.hpp>
+
+using namespace std;
+
+class Client : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  Client()
+      : Node("client")
+  {
+    this->declare_parameter("b", vector<double>{1.0, 2.0, 3.0});
+    this->declare_parameter("A", vector<double>{
+                                     1.0, 2.0, 3.0,
+                                     4.0, 5.0, 6.0,
+                                     7.0, 8.0, 9.0});
+    this->declare_parameter("debug", false);
 
-  printf("hello world linear_algebra_service package I am the client\n");
+    auto b = this->get_parameter("b").as_double_array();
+    auto A = this->parseMatrix(this->get_parameter("A").as_double_array(), b.size(), b.size());
+
+    if (this->get_parameter("debug").as_bool()) {
+      this->debugLog(A, b);
+    }
+  }
+
+private:
+  vector<vector<double>> parseMatrix(const vector<double> & matrix, unsigned rows, unsigned cols)
+  {
+    if (matrix.size() != rows * cols) {
+      throw runtime_error("Matrix size does not match specified dimensions.");
+    }
+    vector<vector<double>> parsed_matrix(rows, vector<double>(cols));
+    for (unsigned i = 0; i < rows; ++i) {
+      for (unsigned j = 0; j < cols; ++j) {
+        parsed_matrix[i][j] = matrix[i * cols + j];
+      }
+    }
+    return parsed_matrix;
+  }
+
+  void debugLog(vector<vector<double>>  A, const vector<double> & b)
+  {
+    printf("Debug Log:\n");
+    printf("Matrix A:\n");
+    for (const auto & row : A) {
+      for (const auto & value : row) {
+        printf("%f ", value);
+      }
+      printf("\n");
+    }
+    printf("Vector b: ");
+    for (const auto & value : b) {
+      printf("%f ", value);
+    }
+    printf("\n");
+  }
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(make_shared<Client>());
+  rclcpp::shutdown();
   return 0;
 }
