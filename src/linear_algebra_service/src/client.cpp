8c3ed2dc5824b83a48ea03a0dc90d3a1c4eed31d
Dimitris <dimitrisgegas01@gmail.com>
2025-07-21 11:33:21 +0900

    First dummy client and server

---
+++ src/linear_algebra_service/src/client.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world linear_algebra_service package I am the client\n");
+  return 0;
+}
