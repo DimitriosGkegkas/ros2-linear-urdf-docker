b5cc1a17550999f26cad55e8624fb45e661710df
Dimitris <dimitrisgegas01@gmail.com>
2025-07-22 12:33:45 +0900

    Defined Server and Client with custom interface to solve the Least Square

--- src/linear_algebra_service/src/client.cpp
+++ src/linear_algebra_service/src/client.cpp
@@ -1,9 +1,9 @@
-#include <chrono>
-#include <functional>
-#include <string>
 #include <vector>
+#include <Eigen/Dense>
 
-#include <rclcpp/rclcpp.hpp>
+#include "geometry_msgs/msg/point.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "linear_algebra_interfaces/srv/get_least_squares.hpp"
 
 using namespace std;
 
@@ -13,51 +13,124 @@ public:
   Client()
       : Node("client")
   {
+    // Parameter declaration
     this->declare_parameter("b", vector<double>{1.0, 2.0, 3.0});
     this->declare_parameter("A", vector<double>{
                                      1.0, 2.0, 3.0,
                                      4.0, 5.0, 6.0,
                                      7.0, 8.0, 9.0});
-    this->declare_parameter("debug", false);
 
     auto b = this->get_parameter("b").as_double_array();
-    auto A = this->parseMatrix(this->get_parameter("A").as_double_array(), b.size(), b.size());
+    auto A = this->get_parameter("A").as_double_array();
 
-    if (this->get_parameter("debug").as_bool()) {
-      this->debugLog(A, b);
+    client_ = this->create_client<linear_algebra_interfaces::srv::GetLeastSquares>("get_least_squares");
+    publisher_ = this->create_publisher<geometry_msgs::msg::Point>("least_squares_solution", 10);
+
+    auto leastSquareSolution = this->getLeastSquares(A, b);
+    if (leastSquareSolution)
+    {
+      RCLCPP_INFO(this->get_logger(), "Least squares solution: [%f, %f, %f]",
+                  leastSquareSolution->x, leastSquareSolution->y, leastSquareSolution->z);
+    }
+    else
+    {
+      RCLCPP_WARN(this->get_logger(), "Least squares failed");
     }
   }
 
 private:
-  vector<vector<double>> parseMatrix(const vector<double> & matrix, unsigned rows, unsigned cols)
+  rclcpp::Client<linear_algebra_interfaces::srv::GetLeastSquares>::SharedPtr client_;
+  rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
+
+  optional<geometry_msgs::msg::Point> getLeastSquares(const vector<double> &A, const vector<double> &b)
   {
-    if (matrix.size() != rows * cols) {
-      throw runtime_error("Matrix size does not match specified dimensions.");
-    }
-    vector<vector<double>> parsed_matrix(rows, vector<double>(cols));
-    for (unsigned i = 0; i < rows; ++i) {
-      for (unsigned j = 0; j < cols; ++j) {
-        parsed_matrix[i][j] = matrix[i * cols + j];
+
+    this->logInfo(A, b);
+
+    // Creating request
+    auto request = std::make_shared<linear_algebra_interfaces::srv::GetLeastSquares::Request>();
+    request->a = A;
+    request->b = b;
+
+    // Wait for the service to be available
+    while (!client_->wait_for_service(1s))
+    {
+      if (!rclcpp::ok())
+      {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+        return std::nullopt;
       }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service not available, waiting again...");
     }
-    return parsed_matrix;
+
+    // Call the service asynchronously
+    auto result = client_->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+      // Process the response
+      auto response = result.get();
+      Eigen::Vector3f x_t(
+          response->x_t.x,
+          response->x_t.y,
+          response->x_t.z);
+
+      Eigen::Vector3f t(
+          response->t.translation.x,
+          response->t.translation.y,
+          response->t.translation.z);
+
+      Eigen::Quaternionf q(
+          response->t.rotation.w, // Note: Eigen constructor order is (w, x, y, z)
+          response->t.rotation.x,
+          response->t.rotation.y,
+          response->t.rotation.z);
+
+      // Apply the inverse rotation and translation to the point to get x out of x_t
+      Eigen::Vector3f x = q.inverse() * (x_t - t);
+
+      // Create the Point message
+      geometry_msgs::msg::Point point;
+      point.x = x.x();
+      point.y = x.y();
+      point.z = x.z();
+      return point;
+    }
+    else
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service get_least_squares");
+      return std::nullopt;
+    }
   }
 
-  void debugLog(vector<vector<double>>  A, const vector<double> & b)
+  void logInfo(const std::vector<double> &A, const std::vector<double> &b)
   {
-    printf("Debug Log:\n");
-    printf("Matrix A:\n");
-    for (const auto & row : A) {
-      for (const auto & value : row) {
-        printf("%f ", value);
+    std::stringstream ss;
+
+    ss << "Requesting Least Squares with parameters:\n";
+    ss << "Matrix A:\n";
+    for (size_t i = 0; i < A.size(); i += 3)
+    {
+      if (i + 2 < A.size())
+      {
+        ss << A[i] << " " << A[i + 1] << " " << A[i + 2] << "\n";
       }
-      printf("\n");
+      else
+      {
+        ss << "Incomplete row at end of A vector!\n";
+        break;
+      }
     }
-    printf("Vector b: ");
-    for (const auto & value : b) {
-      printf("%f ", value);
+
+    ss << "Vector b: ";
+    for (const auto &value : b)
+    {
+      ss << value << " ";
     }
-    printf("\n");
+    ss << "\n";
+
+    RCLCPP_INFO_STREAM(this->get_logger(), ss.str());
   }
 };
 
--- src/linear_algebra_service/src/server.cpp
+++ src/linear_algebra_service/src/server.cpp
@@ -1,10 +1,86 @@
-#include <cstdio>
+#include <vector>
+#include <cinttypes>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <Eigen/Dense>
 
-int main(int argc, char ** argv)
+#include <rclcpp/rclcpp.hpp>
+#include <linear_algebra_interfaces/srv/get_least_squares.hpp>
+
+using namespace std;
+
+class Server : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  Server() : Node("server")
+  {
+    service_ = this->create_service<linear_algebra_interfaces::srv::GetLeastSquares>(
+        "get_least_squares", Server::handleGetLeastSquares);
 
-  printf("hello world linear_algebra_service package I am the server\n");
+    RCLCPP_INFO(this->get_logger(), "Service 'get_least_squares' is ready.");
+  }
+
+private:
+  rclcpp::Service<linear_algebra_interfaces::srv::GetLeastSquares>::SharedPtr service_;
+
+  static void
+  handleGetLeastSquares(
+      const std::shared_ptr<rmw_request_id_t> request_header,
+      const std::shared_ptr<linear_algebra_interfaces::srv::GetLeastSquares::Request> request,
+      std::shared_ptr<linear_algebra_interfaces::srv::GetLeastSquares::Response> response)
+  {
+    (void)request_header;
+
+    Eigen::MatrixXf A(request->b.size(), 3);
+    Eigen::VectorXf b(request->b.size());
+
+    for (size_t i = 0; i < request->b.size(); ++i)
+    {
+      b(i) = request->b[i];
+      for (size_t j = 0; j < 3; ++j)
+      {
+        A(i, j) = request->a[i * 3 + j];
+      }
+    }
+
+    RCLCPP_INFO(rclcpp::get_logger("linear_algebra_service"), "Received request for least squares with A size [%ld, %ld] and b size [%ld]",
+                A.rows(), A.cols(), b.size());
+
+    Eigen::VectorXf sol = (A.transpose() * A).ldlt().solve(A.transpose() * b);
+    std::stringstream ssol;
+    ssol << sol.transpose();
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("linear_algebra_service"), "Least Square Solution: " << ssol.str());
+
+    Eigen::Vector3f translation = Eigen::Vector3f::Random(3);
+    response->t.translation.x = translation[0];
+    response->t.translation.y = translation[1];
+    response->t.translation.z = translation[2];
+
+    Eigen::Quaternionf q = Eigen::Quaternionf::UnitRandom();
+    response->t.rotation.x = q.x();
+    response->t.rotation.y = q.y();
+    response->t.rotation.z = q.z();
+    response->t.rotation.w = q.w();
+
+    // Apply translation and rotation to the solution
+    auto x_t = q * sol + translation;
+
+    response->x_t.x = x_t[0];
+    response->x_t.y = x_t[1];
+    response->x_t.z = x_t[2];
+
+    std::stringstream sx_t;
+    sx_t << x_t.transpose();
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("linear_algebra_service"), "Transformed Least Square Solution: " << sx_t.str());
+  }
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(make_shared<Server>());
+  rclcpp::shutdown();
   return 0;
 }
