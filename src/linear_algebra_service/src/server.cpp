27aacc37bee01e61f0bd4349d03706aea34581b2
Dimitris <dimitrisgegas01@gmail.com>
2025-07-23 18:43:44 +0900

    Clean up code at server node

--- src/linear_algebra_service/src/server.cpp
+++ src/linear_algebra_service/src/server.cpp
@@ -1,14 +1,15 @@
-#include <vector>
-#include <cinttypes>
 #include <memory>
-#include <cstdlib>
-#include <ctime>
-#include <random>
+#include <vector>
 #include <Eigen/Dense>
-#include "geometry_msgs/msg/point.hpp"
 
+#include <ctime>
+#include <cstdlib>
+
+#include "geometry_msgs/msg/point.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <linear_algebra_interfaces/srv/get_least_squares.hpp>
+
+
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -41,10 +42,15 @@ private:
       std::shared_ptr<linear_algebra_interfaces::srv::GetLeastSquares::Response> response)
   {
     (void)request_header;
+    if (request->a.size() != request->b.size() * 3)
+    {
+      RCLCPP_ERROR(this->get_logger(), "Invalid sizes for A and b vectors");
+      return;
+    }
 
+    // We assume A is a 3-column matrix and b is a vector of size n
     Eigen::MatrixXf A(request->b.size(), 3);
     Eigen::VectorXf b(request->b.size());
-
     for (size_t i = 0; i < request->b.size(); ++i)
     {
       b(i) = request->b[i];
@@ -53,16 +59,19 @@ private:
         A(i, j) = request->a[i * 3 + j];
       }
     }
-
     RCLCPP_INFO(this->get_logger(), "Received request for least squares with A size [%ld, %ld] and b size [%ld]",
                 A.rows(), A.cols(), b.size());
 
+    // Perform least squares solution
     Eigen::VectorXf sol = (A.transpose() * A).ldlt().solve(A.transpose() * b);
-    std::stringstream ssol;
-    ssol << sol.transpose();
-    RCLCPP_INFO_STREAM(this->get_logger(), "Least Square Solution: " << ssol.str());
+    RCLCPP_INFO(this->get_logger(), "Least Squares Solution: [%f, %f, %f]",
+                sol(0), sol(1), sol(2));
 
     Eigen::Vector3f translation = Eigen::Vector3f::Random(3);
+    RCLCPP_INFO_STREAM(this->get_logger(), "Random Translation: "
+                                               << translation[0] << ", "
+                                               << translation[1] << ", "
+                                               << translation[2]);
     response->t.translation.x = translation[0];
     response->t.translation.y = translation[1];
     response->t.translation.z = translation[2];
@@ -75,25 +84,25 @@ private:
 
     // Apply translation and rotation to the solution
     auto x_t = q * sol + translation;
-
     response->x_t.x = x_t[0];
     response->x_t.y = x_t[1];
     response->x_t.z = x_t[2];
-
-    std::stringstream sx_t;
-    sx_t << x_t.transpose();
-    RCLCPP_INFO_STREAM(this->get_logger(), "Transformed Least Square Solution: " << sx_t.str());
+    RCLCPP_INFO_STREAM(this->get_logger(), "Transformed Least Square Solution: "
+                                               << response->x_t.x << ", "
+                                               << response->x_t.y << ", "
+                                               << response->x_t.z);
   }
 
   void handleSubscription(const geometry_msgs::msg::Point::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "Received least squares solution: [%f, %f, %f]",
+    RCLCPP_INFO(this->get_logger(), "Received Back least squares solution: [%f, %f, %f]",
                 msg->x, msg->y, msg->z);
   }
 };
 
 int main(int argc, char **argv)
 {
+  std::srand(static_cast<unsigned>(std::time(nullptr)));
   rclcpp::init(argc, argv);
   rclcpp::spin(make_shared<Server>());
   rclcpp::shutdown();
